1bcfb880560a8db66dd935547a4adeaab385a5a4
franckletestu <letestu.franck@gmail.com>
2021-08-16 23:38:43 +0200

    Initial

---
+++ Essai_Streamlit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.write("## Test")
+
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+st.caption('This is a small text')
+
+st.sidebar.header("About")
